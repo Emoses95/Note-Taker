1d4ad6998a370c1b38e74c777ae8cea8261ac2e0
Emoses95 <moses.e.collins@gmail.com>
2023-01-26 20:50:36 -0800

    connected front end to backend

---
+++ routes/db.js
@@ -0,0 +1,30 @@
+const db= require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+db.post('/', (req,res) => {
+    readFromFile('.db/db.json').then((data)=> res.json(JSON.parse(data)))
+})
+
+db.post('/',(req,res)=>{
+    console.log(req.body);
+
+    const{username, topic, tip}= req.body;
+
+    if (req.body) {
+        const newDb = {
+          username,
+          tip,
+          topic,
+          tip_id: uuidv4(),
+        };
+
+        readAndAppend(newDb, './db/tips.json');
+    res.json(`db added successfully 🚀`);
+} else {
+    res.error('Error in adding tip');
+  }
+
+
+})
+module.exports= db;
